6c6166f02074f803a1e461c21f0c305b13e82fdb
sunjiaqi544300 <2412772489@qq.com>
2018-03-06 09:28:57 +0800

    food修改

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,6 +1,17 @@
 <template>
 	<div v-show="" class="">
-		
+ 1、扎实的JavaScript的基础；
+ 2、熟练掌握Html+css布局及效果实现，可处理常见浏览器兼容问题，能开发兼容手机浏览器的网页页面；
+ 3、掌握Angular/React/Vue 一种或多种框架；
+ 4、熟练使用常用打包框架，webpack gulp 等；
+ 5、与后端开发人员配合，高质量完成网站前端开发工作；
+ 6、能够根据需求，负责公司pc，移动端站点的功能开发、调试、上线及更新升级；
+ 7、有实际项目开发经验，面试时请带作品。
+ 任职要求：
+ 1、熟悉H5，CSS3，JavaScript；
+ 2、熟悉vuejsJS/ jQuery/ AngularJS；
+ 3、熟悉git。
+ 4、大专及以上学历；三年以上工作经验；
 	</div>
 </template>
 
